4db676549a4502aa9d3c8b1b6ad95a32cf51d936
njagua <njagua.gma@gmail.com>
2025-02-05 13:10:06 +0300

    checking answers

--- quiz.js
+++ quiz.js
@@ -1,1 +1,31 @@
+/* 1st create an array
+It will store all the correct answers of the quiz
+ 
+How are we going to store these?
+we are going to store them as the actual values that each input
+field has
+*/
+const correctAnswers = ["A", "A", "B", "A"];
 
+// attach an event listener to the form
+
+const form = document.querySelector(".quiz-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let score = 0;
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+  // check answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
+  });
+
+  console.log(score);
+});
