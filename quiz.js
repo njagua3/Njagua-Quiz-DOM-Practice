e94bcf760852496fa7ed2581cb82df2b41324271
njagua <njagua.gma@gmail.com>
2025-02-18 09:23:18 +0300

    window object

--- quiz.js
+++ quiz.js
@@ -14,7 +14,7 @@ const correctAnswers = ["A", "A", "B", "A"];
 
 const form = document.querySelector(".quiz-form");
 
-result = document.querySelector(".result");
+const result = document.querySelector(".result");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -33,7 +33,7 @@ form.addEventListener("submit", (e) => {
   });
 
   //console.log(score);
-  //show result
+  //show result on page
   scrollTo(0, 0);
   result.querySelector("span").textContent = `${score}%`;
   result.classList.remove("d-none");
@@ -42,6 +42,21 @@ form.addEventListener("submit", (e) => {
 // STEP 2
 // DISPLAYING THE SCORE
 
-result = document.querySelector(".result");
+//result = document.querySelector(".result");
 
 // update the result with the score
+/*
+Window object - Its the global object in frontend JS
+
+Everything you do in fe js is stored on this window
+
+When we use a property/method from the window object we don't
+have to write it out window. 
+
+Example
+console.log('hello');
+is the same as 
+windows.console.log('hello')
+
+
+*/
